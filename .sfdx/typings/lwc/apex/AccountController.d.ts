ed938c4fc8f2c2dc3399ff4523fdc39f0e53666b
Venugopal <107802359+Venugopal116@users.noreply.github.com>
2022-07-24 10:32:19 +0530

    test

--- .sfdx/typings/lwc/apex/AccountController.d.ts
+++ .sfdx/typings/lwc/apex/AccountController.d.ts
@@ -4,6 +4,9 @@ declare module "@salesforce/apex/AccountController.getAccounts" {
 declare module "@salesforce/apex/AccountController.getAccountList" {
   export default function getAccountList(): Promise<any>;
 }
+declare module "@salesforce/apex/AccountController.getAccountListForSearch2" {
+  export default function getAccountListForSearch2(param: {accSearch: any}): Promise<any>;
+}
 declare module "@salesforce/apex/AccountController.getAccountListForSearch" {
   export default function getAccountListForSearch(param: {searchKey: any}): Promise<any>;
 }
--- force-app/main/default/lwc/wireToProperty/wireToProperty.js
+++ force-app/main/default/lwc/wireToProperty/wireToProperty.js
@@ -1,7 +1,20 @@
-import { LightningElement ,wire} from 'lwc';
+import { LightningElement ,wire,track} from 'lwc';
 import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+import getAccountList2 from '@salesforce/apex/AccountController.getAccountListForSearch2';
 export default class WireToProperty extends LightningElement {
-    
-    @wire(getAccountList) accounts;
+     @track accSearch ='';
 
+    
+    @wire(getAccountList) accounts; 
+
+    @wire(getAccountList2, {accSearch : '$accSearch'}) accounts2;
+
+
+    inputHandle(event){
+        const searchkey=event.target.value;
+        this.accSearch=searchkey;
+        console.log('button clicked ,accasearch value is========      ',this.accSearch);
+        console.log('acc2 data========      ',this.accounts2);
+        console.log('local search key========      ',searchkey);
+    }
 }
